768174cff076bfb4c458aa6ff6cb507715982a0f
Matnit02 <mateusz.s921@gmail.com>
2023-05-15 16:07:45 +0200

    Bugfix - pdf commented in urls, because it does not work

--- Projekt/crab/crab/cli/templates/urls.py
+++ Projekt/crab/crab/cli/templates/urls.py
@@ -1,4 +1,4 @@
-from views import home, hello, request, return_error, xml, pdf, image
+from views import home, hello, request, return_error, xml, image
 import re
 
 
@@ -6,7 +6,7 @@ url_patterns = [
     ('/', home),
     ('/xml', xml),
     ('/image', image),
-    ('/pdf', pdf),
+    # ('/pdf', pdf), Not working correctly at the time being
     ('/request', request),
     ('/error_500', return_error),
     (re.compile(r'^/hello/(?P<name>\w+)/$'), hello),
